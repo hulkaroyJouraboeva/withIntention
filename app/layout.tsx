c8cb8171a5f3881af94010d39c6d029565b0b72c
hulkaroy <hulkaroy.jouraboeva@rutgers.edu>
2026-02-28 22:52:33 -0500

    chore: Remove DataProvider import from layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter, Outfit } from 'next/font/google'
 import './globals.css'
 import { Sidebar } from '@/components/layout/Sidebar'
 import { Topbar } from '@/components/layout/Topbar'
-import { DataProvider } from '@/components/providers/DataProvider'
+
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 const outfit = Outfit({ subsets: ['latin'], variable: '--font-outfit' })
